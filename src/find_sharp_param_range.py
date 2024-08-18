5dd6c354ebd256e54d2b00813d0272c171ae60bd
dorbarber79 <65215272+dorbarber79@users.noreply.github.com>
2024-08-18 16:18:19 +0300

    Add files via upload
    add script to find sharpness parameters range and step

---
+++ src/find_sharp_param_range.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+from process_raw import DngFile
+from wand.image import Image
+import os
+
+range_min = 5 #range  = 10 --> 1 pix
+range_max = 300
+range_step = 5
+
+sharpness_dng_path = r'../images/Sharpness/'
+sharpness_processed_root = r'../processed_images/'
+hdr_image_files = (r'a0020-jmac_MG_6225',r'a1781-LS051026_day_10_LL003',r'a0410-jmac_DSC2754',r'a0568-_MG_1090')
+
+for hdr_image_file in hdr_image_files:
+    sharpness_processed_images = sharpness_processed_root + hdr_image_file + '/'
+    isExist = os.path.exists(sharpness_processed_images)
+    if not isExist:
+        os.mkdir(sharpness_processed_images)
+    dng = DngFile.read(sharpness_dng_path + hdr_image_file + '.dng')
+    raw = dng.raw  # np.uint16
+    raw_8bit = np.uint8(raw >> (dng.bit-8))
+    rgb1 = dng.postprocess()  # demosaicing by rawpy
+    output_path=sharpness_processed_images+ hdr_image_file +".jpg"
+    cv2.imwrite(output_path, rgb1[:, :, ::-1])
+
+    #process with wand
+    for sigma in  range(range_min,range_max,range_step):
+        with Image(filename = output_path) as image:
+             with image.clone() as sharpen:
+                  # Invoke sharpen function with radius = 5 * sigma
+                sharpen.sharpen(min(round(sigma * 5 / 10),90), sigma / 10)
+                # Save the image
+                sharpen.save(filename =output_path+'_sharpen_' + str(sigma) + '.jpg')
